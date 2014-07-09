e92f0b48e82397fd0443c4571401cc59e6050f06
Len <leonard.wu92@alumni.imperial.ac.uk>
2014-07-09 02:16:07 +0100

    Edit text in default route index

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,4 @@
 exports.index = function(req, res){
 	res.writeHead(200);
-	res.end('<html><head><title>Hello</title></head><body><h1>Hello</h1><p>Hello World</p>></body></html>');
+	res.end('<html><head><title>Express</title></head><body><h1>Express</h1><p>Express</p>></body></html>');
 };
