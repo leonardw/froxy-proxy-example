4c37d6b1a4372d7805498a0d90479c0494b8656d
Len <leonard.wu92@alumni.imperial.ac.uk>
2014-07-14 12:01:39 +0100

    Add comments into Express example

--- express-ex.js
+++ express-ex.js
@@ -5,17 +5,19 @@ var express = require('express'),
 var app = express();
 app.set('port', 8000);
 
+// Github URLs proxy to github.com/leonardw/
 app.route('/github/*').get(froxy.proxy({
 	host: 'github.com',
 	translate: ['/github/', '/leonardw/'],
 	debug: true
 }));
 
+// All other URLs proxy to Wikipedia
 app.route('/*').get(froxy.proxy({
 	host: 'en.wikipedia.org',
 	debug: true
 }));
 
 http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port', app.get('port'));
+	console.log('Express server listening on port', app.get('port'));
 });
